be7a7f01181268c91cf3df4749d2148e04c6e05f
TanguyFox <tanguyfox@gmail.com>
2023-06-07 14:42:38 +0200

    Implementation ServiceDistributeur

---
+++ service/DistributeurNoeud.java
@@ -0,0 +1,25 @@
+public class DistributeurNoeud implements ServiceDistributeur {
+
+    private List<ServiceImage> neouds = new ArrayList<>();
+    private int index;
+    public DistributeurNoeud() {
+        this.index = 0;
+    }
+
+    public void enregistrerNoeud(ServiceImage noeud) throws RemoteException {
+        this.neouds.add(noeud);
+    }
+
+    public ServiceImage donnerNoeud() throws RemoteException {
+        if (index < neouds.size()) {
+            ServiceImage n = neouds.get(index);
+            index++;
+            return n;
+        } else {
+            index = 0;
+            ServiceImage n = neouds.get(index);
+            index++;
+            return n;
+        }
+    }
+}
---
+++ service/ServiceDistributeur.java
@@ -0,0 +1,11 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ServiceDistributeur {
+
+    public void enregistrerNoeud(ServiceImage noeud) throws RemoteException;
+
+    public void enregistrerNoeud(ServiceDistributeur noeud) throws RemoteException;
+
+    public ServiceImage donnerNoeud() throws RemoteException;
+}
