b76c33b785ed3921a477d27eb4fb740175cbb6ed
ZHANG Xin <zhang284u@etu.univ-lorraine.fr>
2023-06-07 21:49:49 +0200

    final version v1.1 xin

--- distributeur/MainDistributeur.java
+++ distributeur/MainDistributeur.java
@@ -2,6 +2,8 @@ package distributeur;
 
 import service.ServiceDistributeur;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -11,11 +13,24 @@ public class MainDistributeur {
 
     public static void main(String[] args) throws RemoteException {
 
+        int port = 1099;
+        if(args.length > 0){
+            port = Integer.parseInt(args[0]);
+        }
+
+        InetAddress localHost = null;
+        try {
+            localHost = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        String ip = localHost != null ? localHost.getHostAddress() : "Unknown IP";
+        System.out.println("IP address:"+ip);
 
         DistributeurNoeud dn = new DistributeurNoeud();
         ServiceDistributeur sd = (ServiceDistributeur) UnicastRemoteObject.exportObject(dn, 0);
 
-        Registry reg = LocateRegistry.createRegistry(1099);
+        Registry reg = LocateRegistry.createRegistry(port);
         reg.rebind("distributeur", sd);
 
         System.out.println("EN attente d'une requete");
--- image/MainImage.java
+++ image/MainImage.java
@@ -13,10 +13,24 @@ public class MainImage {
 
     public static void main(String[] args) throws RemoteException, NotBoundException {
 
+        String host = "localhost";
+        int port = 1099;
+
+        if(args.length > 0){
+            host = args[0];
+            if(args.length > 1){
+                port = Integer.parseInt(args[1]);
+            }
+        }
+
+
         DistributeurImage di = new DistributeurImage();
+        System.out.println("IP address: "+di.getInformation());
+
         ServiceImage si = (ServiceImage) UnicastRemoteObject.exportObject(di, 0);
 
-        Registry reg = LocateRegistry.getRegistry(1099);
+        Registry reg = LocateRegistry.getRegistry(host,port);
+
         ServiceDistributeur sd = (ServiceDistributeur) reg.lookup("distributeur");
         sd.enregistrerNoeud(si);
 
